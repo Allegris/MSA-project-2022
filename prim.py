88dd5da857995389cc81106b9b853fba528ef1c5
Allegris <AstridChristiansenCS@gmail.com>
2022-04-15 11:16:59 +0200

    Prim: removed old code and added comments

--- prim.py
+++ prim.py
@@ -3,34 +3,38 @@ import numpy as np
 import project2_linear as pa #pairwise alignment / SP score
 
 
-# PRIM's algorithm for computing a minimum spanning tree for a set of strings, nodes
-# (nodes are the string indices, the actual strings can be accessed as node_strings[i] for i in nodes)
-def old_MST_prim(nodes, node_strings, sub_matrix, gap_cost):
+# Constructs a MST from a set of nodes and cost matrix (as sub_matrix and gap_cost)
+# node_strings is a list of the actual strings
+# nodes is a corresponding numbering of the strings: [0, 1, 2,...]
+def MST_prim(nodes, node_strings, sub_matrix, gap_cost):
+	V = nodes.copy() # The set of nodes
 	# Construct score matrix
 	cost = construct_score_matrix(nodes, node_strings, sub_matrix, gap_cost)
-	mst = [] # Will contain the MST
-	in_MST = [0]
-	not_in_MST = nodes.copy()
-	not_in_MST.remove(0)
-	# We want all nodes in the MST - when we have this, we are done
-	while len(not_in_MST) > 0:
-		min_cost = sys.maxsize # Min edge to MST
-		pair = (-1, -1)
-		# Check all possible edges (pair of nodes)
-		for u in in_MST:
-			for v in not_in_MST:
-				# If the edge has lower cost than min_cost
-				if cost[u][v] < min_cost:
-					min_cost = cost[u][v]
-					pair = (u, v)
-		if pair != (-1, -1):
-			# Add the found edge to MST
-			mst.append(pair)
-			in_MST.append(pair[1])
-			not_in_MST.remove(pair[1])
+	mst = [] # Will contain the MST edges (i.e. the resulting MST)
+	key = [sys.maxsize] * len(V) # The value of the lightest weight connecting a node to the current MST
+	parent = [None] * len(V) # The parent node that a given node was connected to the MST using
+
+	key[0] = 0 # Set the key of node 0 to 0, ensuring that we pick this as the first node
+
+	# While not all nodes are in MST
+	while len(mst) < len(V):
+		# Find the node that can be added to the MST giving
+		# the lowest possible weight addition
+		u = find_min_node(V, mst, key)
+		mst.append(u)
+
+		# Update keys and parents of nodes not in MST, since MST has changed
+		for v in V:
+			if v not in mst and cost[u][v] < key[v]:
+				key[v] = cost[u][v]
+				parent[v] = u
+	# Return MST as edge pairs (u, v)
+	# The edges appear in the order that they are added to the MST
+	mst = [(parent[v], v) for v in mst[1:]] # Remove the first entry, since this is "None node to the root", i.e. (None, 0)
 	return mst
 
 
+# Returns the node in the set *V - mst* with the lowest key value
 def find_min_node(V, mst, key):
 	min_val = sys.maxsize
 	min_node = None
@@ -39,33 +43,6 @@ def find_min_node(V, mst, key):
 			min_node = v
 			min_val = key[v]
 	return min_node
-
-
-def MST_prim(nodes, node_strings, sub_matrix, gap_cost):
-	V = nodes.copy()
-	# Construct score matrix
-	cost = construct_score_matrix(nodes, node_strings, sub_matrix, gap_cost)
-	mst = [] # Will contain the MST edges (i.e. the resulting MST)
-	key = [sys.maxsize] * len(V)
-	parent = [None] * len(V)
-
-	key[0] = 0
-
-	#for _ in range(len(V)):
-	while len(mst) < len(V):
-		u = find_min_node(V, mst, key)
-		mst.append(u)
-
-		for v in V:
-			if v not in mst and cost[u][v] < key[v]:
-				key[v] = cost[u][v]
-				parent[v] = u
-
-	mst = [(parent[v], v) for v in mst[1:]] #remove the first entry, since this is "None node to the root", i.e. (-1, 0)
-	return mst
-
-
-
 
 
 # Construct a score matrix for the string indices in list S (the actual strings are in node_strings)
@@ -95,7 +72,7 @@ def construct_score_matrix(S, node_strings, sub_matrix, gap_cost):
 
 
 ##### Code to run #####
-
+'''
 #sub_matrix = {"A": {"A": 5, "C": 5, "G": 5, "T": 5}, "C": {"A": 5, "C": 5, "G": 5, "T": 5}, "G": {"A": 5, "C": 5, "G": 5, "T": 5}, "T": {"A": 5, "C": 5, "G": 5, "T": 5}}
 sub_matrix = {"A": {"A": 10, "C": 2, "G": 5, "T": 2}, "C": {"A": 2, "C": 10, "G": 2, "T": 5}, "G": {"A": 5, "C": 2, "G": 10, "T": 2}, "T": {"A": 2, "C": 5, "G": 2, "T": 10}}
 gap_cost = 5
@@ -103,7 +80,7 @@ gap_cost = 5
 node_strings = ["AACG", "AAAA", "CCCC", "GGGG"]
 nodes = list(range(len(node_strings)))
 print(MST_prim(nodes, node_strings, sub_matrix, gap_cost))
-
+'''
 
 
 
